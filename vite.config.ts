6b4d55518d183a188c0b60d0a31d2d0dbacf0a2f
Louis Lacoste <moi@louislacoste.fr>
2023-11-24 14:52:32 +0100

    feat: added vitest config for standard typescript file unit tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTest.tsx",
+    globalSetup: "./src/globalSetupTest.ts",
+    threads: true,
+    minThreads: 1,
+    maxThreads: 8,
+    css: true,
+    coverage: {
+      all: true,
+      provider: "v8",
+      include: ["src/**/*.{ts}"],
+      exclude: [
+        "src/**/*.d.ts",
+        "src/**/types.ts",
+        "src/**/*.{tsx}",
+        "src/**/*.test.{ts,tsx}",
+        "src/**/*.spec.{ts,tsx}",
+        "src/setupTest.tsx",
+        "src/globalSetupTest.ts",
+        "src/main.tsx",
+      ],
+    },
+  },
+});
