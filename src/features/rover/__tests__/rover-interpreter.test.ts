eb9cf110bf07cb82ae5345b1c693eb96adb8aadc
IShungite <56173955+IShungite@users.noreply.github.com>
2023-12-21 15:41:05 +0100

    test: ✅ Add rover interpreter tests

--- src/features/rover/__tests__/rover-interpreter.test.ts
+++ src/features/rover/__tests__/rover-interpreter.test.ts
@@ -6,9 +6,11 @@ describe("RoverInterpreter", () => {
 
   test.each([
     ['?', (rover: IRover) => rover],
+    ['!!', (rover: IRover) => rover],
+    ['F', (rover: IRover) => rover.forward()],
     ['FF', (rover: IRover) => rover.forward().forward()],
     ['B', (rover: IRover) => rover.backward()],
-    ['L', (rover: IRover) => rover.turnLeft()],
+    ['LLF', (rover: IRover) => rover.turnLeft().turnLeft().forward()],
     ['R', (rover: IRover) => rover.turnRight()],
   ])("Should interpret the command '%s'", (command, action) => {
     const rover = new RoverBuilder().build();
