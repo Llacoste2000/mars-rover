138e74b6139ded34fffb3406af5dc61ad1112240
IShungite <56173955+IShungite@users.noreply.github.com>
2023-12-22 15:10:24 +0100

    refactor: remove unused code

--- src/features/planet/PlanetWithObstacle.ts
+++ src/features/planet/PlanetWithObstacle.ts
@@ -3,9 +3,7 @@ import { IPlanet } from "./Planet.interface";
 
 // Objet-valeur
 export class PlanetWithObstacle implements IPlanet {
-  constructor(private readonly planet: IPlanet, private readonly obstacles: Position[]) {
-    this.obstacles = obstacles;
-  }
+  constructor(private readonly planet: IPlanet, private readonly obstacles: Position[]) { }
 
   public normalize(position: Position) {
     return this.planet.normalize(position);
