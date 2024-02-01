1f5084e04fec5b4cbb6fba1ef1146d4dadb11002
IShungite <56173955+IShungite@users.noreply.github.com>
2024-02-01 10:16:35 +0100

    add receive function to IProtocolCommunication

--- src/protocolCommunication/ProtocolCommunication.interface.ts
+++ src/protocolCommunication/ProtocolCommunication.interface.ts
@@ -2,5 +2,6 @@ import { Message } from "./Message.interface";
 
 export interface IProtocolCommunication {
   send(message: Message): void;
+  receive(message: string): void;
   onReceiveMessage(callback: (message: Message) => void): void;
 }
